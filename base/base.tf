455978ce628738d1d810ec84b293d32ff2e75d98
chinthirla.vijay <vijay.itblabs@gmail.com>
2020-08-25 21:02:17 +0530

    first commit

---
+++ base/base.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+resource "aws_instance" "base" {
+  ami = "${lookup(var.ami, var.region)}"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = "${var.security_group_ids}"
+}
+resource "aws_eip" "base" {
+  instance = "${aws_instance.base.id}"
+  vpc = true
+}
---
+++ base/variables.tf
@@ -0,0 +1,32 @@
+variable "access_key" {
+  description = "Aws Access key"
+  default = "AKIAWG2S6RCSIOEBEJWS"
+}
+variable "secret_key" {
+  description = "aws secret key"
+  default = "uEs1Tg4DcjI0w3s19bRVSI9jUY7yyEX7K36HFB2e"
+}
+#using variable type string
+variable "region" {
+  type = "string"
+  description = "Region of aws instance"
+  default = "us-east-1"
+}
+#using variable type list
+variable "ami" {
+  description = "aws instance region"
+  type = "map"
+  default = {
+    us-east-1 = "ami-0d729a60"
+    us-west-1 = "ami-7c4b331c"
+  }
+}
+#using variable type list
+variable "region_list" {
+  description = "aws instance region list"
+  default = ["us-east-1a","us-east-1b"]
+}
+variable "security_group_ids" {
+  description = "aws instance security group"
+  default = "sg-ff74d1dd"
+}

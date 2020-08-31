8cd8d0c5773314fc976b0007441494e721a57301
chinthirla.vijay <vijay.itblabs@gmail.com>
2020-08-31 08:50:38 +0530

    first commit

--- web/web.tf
+++ web/web.tf
@@ -1,3 +1,12 @@
 resource "aws_instance" "web" {
   subnet = "${var.env == "production" ? var.prod_subnet : var.dev_subnet}"
 }
+output "public_subnet_id" {
+  value = "${aws_subnet.public.id}"
+}
+output "vpc_id" {
+  value = "${aws_vpc.tfb.id}"
+}
+output "cidr" {
+  value = "${aws_vpc.tfb.cidr_block}"
+}

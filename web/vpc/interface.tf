8237d22709321c3d15d29203d9e1da359a841068
chinthirla.vijay <vijay.itblabs@gmail.com>
2020-09-15 08:05:41 +0530

    first commit

--- web/vpc/interface.tf
+++ web/vpc/interface.tf
@@ -1,11 +1,21 @@
-module "vpc" {
-  source = "github.com/turnbullpress/tf_vpc
+variable "name" {
+ description = "The name of the VPC."
 }
-#############################################################
-module "vpc" {
-  source = "git::https://github.com/turnbullpress/tf_vpc.git?ref=
-  production"
+variable "cidr" {
+ description = "The CIDR of the VPC."
 }
-
+variable "public_subnet" {
+ description = "The public subnet to create."
+}
+variable "enable_dns_hostnames" {
+ description = "Should be true if you want to use private DNS
+ within the VPC"
+ default = true
+}
+variable "enable_dns_support" {
+ description = "Should be true if you want to use private DNS
+ within the VPC"
+ default = true
+}
 
 
--- web/web.tf
+++ web/web.tf
@@ -1,5 +1,18 @@
+provider "aws" {
+ region = "${var.region}"
+}
+provider "aws" {
+  alias = "west"
+  region = "us-west-1"
+}
 resource "aws_instance" "web" {
   subnet = "${var.env == "production" ? var.prod_subnet : var.dev_subnet}"
+}
+module "vpc" {
+  source = "./vpc"
+  name = "web"
+  cidr = "10.0.0.0/16"
+  public_subnet = "10.0.1.0/24"
 }
 output "public_subnet_id" {
   value = "${aws_subnet.public.id}"
